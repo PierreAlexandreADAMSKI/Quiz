abb7ecc48943cbdbc3a3a29adb04ad305d0a19a7
Pierre-Alexandre ADAMSKI <pierre-alexandre.adamski@isen-lille.fr>
2016-01-21 15:09:01 +0100

    lest start + category

--- app/src/main/java/premiereapplication/testautomation/quiz/objects/Category.java
+++ app/src/main/java/premiereapplication/testautomation/quiz/objects/Category.java
@@ -6,4 +6,28 @@ import java.io.Serializable;
  * Created by mb-p_pilou on 21/01/2016.
  */
 public enum Category implements Serializable {
+    CULTURE_GENERALE,
+    CINEMA,
+    MUSIQUE,
+    LITTERATURE,
+    ACTUALITE;
+
+    public String parse(Category category){
+        if (category == CULTURE_GENERALE){
+            return "Culture Générale";
+        }
+        if (category == CINEMA){
+            return "Cinéma";
+        }
+        if (category == MUSIQUE){
+            return "Musique";
+        }
+        if (category == LITTERATURE){
+            return "Littérature";
+        }
+        if (category == ACTUALITE){
+            return "Actualité";
+        }
+        return null;
+    }
 }

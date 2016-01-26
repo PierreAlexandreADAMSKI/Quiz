b4b7767f7deee6b8168eacdb75ad82b836c78d14
Pierre-Alexandre ADAMSKI <pierre-alexandre.adamski@isen-lille.fr>
2016-01-26 16:35:53 +0100

    final version 1 (on fix) : small fixes

--- app/src/main/java/premiereapplication/testautomation/quiz/interfaces/QuizRetrievedListener.java
+++ app/src/main/java/premiereapplication/testautomation/quiz/interfaces/QuizRetrievedListener.java
@@ -1,14 +1,7 @@
 package premiereapplication.testautomation.quiz.interfaces;
 
-import java.util.List;
-
 import premiereapplication.testautomation.quiz.helpers.CategoryHelper;
-import premiereapplication.testautomation.quiz.helpers.CategoryHelper;
-import premiereapplication.testautomation.quiz.helpers.QuizHelper;
 
 public interface QuizRetrievedListener {
-
-	public void onQuizRetrieved(List<CategoryHelper> categoryHelperList);
-	public void onQuizRetrieved(CategoryHelper categoryHelper);
-
+	void onQuizRetrieved(CategoryHelper categoryHelper);
 }

b0150241bbc5093f16a27a06b9e4ea33a98c66f0
achraf59 <madaniachraf@hotmail.fr>
2016-01-27 03:03:35 +0100

    Categories optimisation

--- app/src/main/java/premiereapplication/testautomation/quiz/fragments/FragmentsOfQuizHomeActivity/GlobalListOfQuizFragment.java
+++ app/src/main/java/premiereapplication/testautomation/quiz/fragments/FragmentsOfQuizHomeActivity/GlobalListOfQuizFragment.java
@@ -68,10 +68,10 @@ public class GlobalListOfQuizFragment extends Fragment implements QuizRetrievedL
 
         if(categoryHelper !=null) {
 
-        bindContainers(isDynamicQuiz,categoryHelper.listOfCinemaQuiz,categoryHelper.listOfCultureGeneraleQuiz,
-                        categoryHelper.listOfSportQuiz,categoryHelper.listOfMusiqueQuiz,categoryHelper.listOfLiteratureQuiz,
-                        categoryHelper.listOfDiversQuiz);
-        }
+            bindContainers(isDynamicQuiz,categoryHelper.CINEMA.getList(),categoryHelper.CULTURE_GENERALE.getList(),
+                    categoryHelper.SPORT.getList(),categoryHelper.MUSIQUE.getList(),categoryHelper.LITERATURE.getList(),
+                    categoryHelper.Divers.getList());}
+
 
         mDynamicQuizAsyncTask = null;
         mStaticQuizAsyncTask = null;
@@ -88,25 +88,24 @@ public class GlobalListOfQuizFragment extends Fragment implements QuizRetrievedL
         return globalListOfQuizsFragmentquizFragment;
     }
 
-    public void bindContainers(boolean isDynamiqueQuiz,List<QuizHelper> list1,List<QuizHelper> list2 ,List <QuizHelper> list3,
-                              List<QuizHelper> list4, List<QuizHelper> list5,List<QuizHelper> list6){
+    public void bindContainers(boolean isDynamiqueQuiz,List<QuizHelper>...list){
 
-        ListOfQuizFragment frag= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Cinema",list1);
+        ListOfQuizFragment frag= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Cinema",list[0]);
         getFragmentManager().beginTransaction().add(R.id.container1, frag).commit();
 
-        ListOfQuizFragment frag2= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Culture Generale",list2);
+        ListOfQuizFragment frag2= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Culture Generale",list[1]);
         getFragmentManager().beginTransaction().add(R.id.container2, frag2).commit();
 
-        ListOfQuizFragment frag3= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Sport",list3);
+        ListOfQuizFragment frag3= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Sport",list[2]);
         getFragmentManager().beginTransaction().add(R.id.container3, frag3).commit();
 
-        ListOfQuizFragment frag4= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Musique",list4);
+        ListOfQuizFragment frag4= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Musique",list[3]);
         getFragmentManager().beginTransaction().add(R.id.container4, frag4).commit();
 
-        ListOfQuizFragment frag5= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Literature",list5);
+        ListOfQuizFragment frag5= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Literature",list[4]);
         getFragmentManager().beginTransaction().add(R.id.container5, frag5).commit();
 
-        ListOfQuizFragment frag6= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Divers",list6);
+        ListOfQuizFragment frag6= ListOfQuizFragment.getInstance(isDynamiqueQuiz,"Divers",list[5]);
         getFragmentManager().beginTransaction().add(R.id.container6, frag6).commit();
 
 
@@ -117,3 +116,4 @@ public class GlobalListOfQuizFragment extends Fragment implements QuizRetrievedL
 
 
 }
+
--- app/src/main/java/premiereapplication/testautomation/quiz/helpers/CategoryHelper.java
+++ app/src/main/java/premiereapplication/testautomation/quiz/helpers/CategoryHelper.java
@@ -1,6 +1,5 @@
 package premiereapplication.testautomation.quiz.helpers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import premiereapplication.testautomation.quiz.objects.Category;
@@ -10,24 +9,24 @@ import premiereapplication.testautomation.quiz.objects.Category;
  */
 public class CategoryHelper {
 
-    public List<QuizHelper> listOfCinemaQuiz=new ArrayList<QuizHelper>();
-    public List<QuizHelper> listOfCultureGeneraleQuiz=new ArrayList<QuizHelper>();
-    public List<QuizHelper> listOfSportQuiz=new ArrayList<QuizHelper>();
-    public List<QuizHelper> listOfMusiqueQuiz=new ArrayList<QuizHelper>();
-    public List<QuizHelper> listOfLiteratureQuiz=new ArrayList<QuizHelper>();
-    public List<QuizHelper> listOfDiversQuiz=new ArrayList<QuizHelper>();
 
-  public CategoryHelper(List<QuizHelper> listOfCinemaQuiz,List<QuizHelper> listOfCultureGeneraleQuiz,List<QuizHelper> listOfSportQuiz,
-                        List<QuizHelper> listOfMusiqueQuiz,List<QuizHelper> listOfLiteratureQuiz,List<QuizHelper> listOfDiversQuiz){
+    public Category CINEMA=Category.CINEMA;
+    public Category CULTURE_GENERALE=Category.CULTURE_GENERALE;
+    public Category SPORT=Category.SPORT;
+    public Category MUSIQUE=Category.MUSIQUE;
+    public Category LITERATURE=Category.LITTERATURE;
+    public Category Divers=Category.DIVERS;
 
-    this.listOfCinemaQuiz=listOfCinemaQuiz;
-    this.listOfCultureGeneraleQuiz=listOfCultureGeneraleQuiz;
-    this.listOfSportQuiz=listOfSportQuiz;
-    this.listOfMusiqueQuiz=listOfMusiqueQuiz;
-    this.listOfLiteratureQuiz=listOfLiteratureQuiz;
-    this.listOfDiversQuiz=listOfDiversQuiz;
+
+    public CategoryHelper(Category...categories){
+
+        this.CINEMA=categories[0];
+        this.CULTURE_GENERALE=categories[1];
+        this.SPORT=categories[2];
+        this.MUSIQUE=categories[3];
+        this.LITERATURE=categories[4];
+        this.Divers=categories[5];
     }
-
 
 
 }
